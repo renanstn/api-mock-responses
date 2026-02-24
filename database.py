fc4a114f6a9aa00c0fa50ba88846219c0f029212
Renan Santana <renan_ota@hotmail.com>
2026-02-24 10:03:17 -0300

    Rename SQLALCHEMY_DATABASE_URL to DATABASE_URL

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from decouple import config
 
 
-SQLALCHEMY_DATABASE_URL = config(
+DATABASE_URL = config(
     "DATABASE_URL", default="postgresql://postgres:postgres@db/postgres"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
